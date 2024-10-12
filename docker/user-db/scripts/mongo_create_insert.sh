ff77ec989719bb38f3d7bb6628f28c6a05994e50
f3lin <alex@jmetio.de>
2024-10-12 07:10:26 +0200

    wip: ci

--- docker/user-db/scripts/mongo_create_insert.sh
+++ docker/user-db/scripts/mongo_create_insert.sh
@@ -1,9 +1,30 @@
+##!/usr/bin/env bash
+#
+#SCRIPT_DIR=$(dirname "$0")
+#
+#mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db/
+#
+#FILES=$SCRIPT_DIR/*-create.js
+#for f in $FILES; do mongo localhost:27017/users $f; done
+#
+#FILES=$SCRIPT_DIR/*-insert.js
+#for f in $FILES; do mongo localhost:27017/users $f; done
+
+
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname "$0")
 
+# Start MongoDB in the background
 mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db/
 
+# Wait for MongoDB to be ready
+until mongo --eval "print(\"waited for connection\")" 127.0.0.1:27017; do
+    echo "Waiting for MongoDB to start..."
+    sleep 2
+done
+
+# Run your MongoDB scripts
 FILES=$SCRIPT_DIR/*-create.js
 for f in $FILES; do mongo localhost:27017/users $f; done
 
